151f4e2eab6195f8d79d065c5081ebaa90ca2d46
shahriarKabir44 <shahriar1904@cseku.ac.bd>
2026-01-14 23:34:42 +0600

    further fixes

--- index.js
+++ index.js
@@ -1,54 +1,83 @@
-const XLSX = require('xlsx');
+const fs = require('fs')
 const path = require('path');
 const ExcelJS = require('exceljs');
 let dir = "D:\\Shourov\\office\\documents\\Prime-Asia\\exam";
 let fileName = "Pau-Exam-Data-after142.xlsx";
 let tabName = "main";
-let workbook = null;
-const startCellNo = "AQ5";
-const endCellNo = "BN100";
+let startCellNo = "AQ4";
+let endCellNo = "BN542549";
+//let endCellNo = "BN2549";
 
-
+let chunkSize = 25000;
+let outputDir = "D:\\Shourov\\office\\documents\\Prime-Asia\\exam\\exam-parts";
+let outputFileTemplate = "Pau-Result-Part-"
 async function readLargeFile() {
 
     let [startRow, startCol] = getRowColNum(startCellNo);
     let [endRow, endCol] = getRowColNum(endCellNo);
 
     // Use the stream reader
+    console.log("Loading Excel File!");
     const workbookReader = new ExcelJS.stream.xlsx.WorkbookReader(path.join(dir, fileName));
 
     for await (const worksheetReader of workbookReader) {
         if (worksheetReader.name !== tabName) continue;
+        let stream = null;
+        let rowNum = 0;
+        let chunkNo = 1;
+        let headerRow = "";
         for await (const row of worksheetReader) {
-            // 'row.values' gives you the data for the current row
-            // You can filter your specific range here manually
-            if (row.number >= startRow && row.number <= endRow) {
-                console.log(`Row ${row.number}:`, row.values[startCol]);
-                let rowData = "";
-                for (let i = startCol; i <= endCol; i++) {
-                    let cell = row.values[i]
-                    try {
-                        let temp = (cell);
-                        if (temp['result']) rowData += temp.result;
-                        else rowData += cell
-                    } catch (error) {
-                        rowData += cell ?? " ";
-
-                    }
-
-                    if (i < endCol) rowData += ","
-                }
-                console.log(rowData)
-                break;
+            if (row.number < startRow) continue;
+            if (row.number > endRow) break;
+            let values = [];
+            for (let i = startCol; i <= endCol; i++) {
+                values.push(row.getCell(i).value);
             }
 
-            // Stop early if you only need a specific range at the top
-            if (row.number > 5) break;
-        }
+            if (row.number == startRow) {
+                headerRow = getCsvRow(values);
+            }
+            if (rowNum % chunkSize == 0) {
+                if (stream) stream.end();
+                console.log(`Processing Chunk ${chunkNo}`);
+                stream = fs.createWriteStream(path.join(outputDir, `${outputFileTemplate}${chunkNo}.csv`));
+                stream.write(headerRow)
+                chunkNo++;
+                rowNum++;
+                if (row.number == startRow) continue;
+            }
 
+            let rowData = getCsvRow(values);
+            if (rowData.includes('object')) {
+                rowData = getCsvRow(values);
+            }
+            stream.write(rowData);
+            rowNum++;
+
+
+        }
+        if (stream) stream.end();
     }
 }
 
+function getCsvRow(row) {
+    let rowData = "";
+    for (let i = 0; i < row.length; i++) {
+        let cell = row[i]
+        try {
+            let temp = (cell);
+
+            if (temp != null && typeof temp == 'object' && temp['result'] != null) rowData += temp.result;
+            else rowData += cell ?? " ";
+        } catch (error) {
+            rowData += cell ?? " ";
+
+        }
+
+        if (i < row.length - 1) rowData += ","
+    }
+    return rowData + '\n';
+}
 
 function columnToNumber(columnName) {
     let number = 0;
